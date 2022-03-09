19e96995485a4a83d203050c92de164c278b8a5c
Ozodbek123 <100948926+Ozodbek123@users.noreply.github.com>
2022-03-09 22:22:09 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var x = prompt("Birinchi sonni kiriting: ")
-var a = prompt("Amalni kiriting ( + ), ( - ), ( * ), ( /    ): ")
+var a = prompt("Amalni kiriting (  +  ), (  -  ), (  *  ), (  /  ): ")
 var y = prompt("Ikkinchi kiriting: ")
 
 document.write( '<p>' + x + '  ' + a + '  ' + y + '  ' + ' = ' + eval (x + a + y) );
